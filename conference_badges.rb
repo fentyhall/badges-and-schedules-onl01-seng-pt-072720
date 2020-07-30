1318c9610ac1bf114483ee5bb96edd033ae9aa78
Fenty Hall <fentymhall@gmail.com>
2020-07-30 16:50:20 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -16,3 +16,13 @@ def printer(attendees)
   batch_array.each {|x| puts x}
   rooms_array.each {|x| puts x}
 end
+
+# def printer(attendees)
+#   batch_badge_creator(attendees).each do |badge|
+#     puts badge
+#   end
+
+#   assign_rooms(attendees).each do |assignment|
+#     puts assignment
+#   end
+# end
